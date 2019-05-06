37ec1d8c7a3c820c67e965e2cbaaca4c7672f329
Anderson Schmidt <schmidtanderson@hotmail.com>
2019-05-06 20:54:15 -0300

    Started adding Authentication

--- app.js
+++ app.js
@@ -6,12 +6,17 @@ var express        = require("express"),
     mongoose       = require("mongoose"),
     methodOverride = require("method-override"),
     fs             = require("fs"),
-    Idea           = require("./models/idea");
-    Comment        = require("./models/comment");
+    expressSession = require("express-session"),
+    passport       = require("passport"),
+    LocalStrategy  = require("passport-local"),
+    Idea           = require("./models/idea"),
+    Comment        = require("./models/comment"),
+    User           = require("./models/user");
 
+// MONGO CONNECTION
 mongoose.connect("mongodb://localhost:27017/lightbulb", {useNewUrlParser: true});
 
-
+// MULTER SETTINGS
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'public/images');
@@ -22,12 +27,30 @@ var storage = multer.diskStorage({
 });
 var upload = multer({ storage: storage });
 
+// GENERAL APP SETTINGS
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(favicon(__dirname + '/favicon.ico')); 
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 
+// AUTHENTICATION SETTINGS
+app.use(expressSession({
+    secret: "My app is going to be famous!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Custom Middleware for every template
+app.use(function(req, res, next){
+    res.locals.currentUser =  req.user;
+    next();
+});
 
 //MAIN PAGE
 app.get("/", function(req, res){
@@ -60,12 +83,12 @@ app.get("/ideas", function(req, res){
 });
 
 //NEW IDEA
-app.get("/ideas/new", function(req, res){
+app.get("/ideas/new", isLoggedIn, function(req, res){
     res.render("ideas/new");
 });
 
 //CREATE IDEA
-app.post("/ideas", upload.single("image"), function(req, res){
+app.post("/ideas", isLoggedIn, upload.single("image"), function(req, res){
     var date = new Date();
     req.body.idea.date = date.getDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes();
 
@@ -140,7 +163,7 @@ app.delete("/ideas/:id", function(req, res){
 });
 
 //NEW COMMENT
-app.get("/ideas/:id/comments/new", function(req, res){
+app.get("/ideas/:id/comments/new", isLoggedIn, function(req, res){
     Idea.findById(req.params.id, function(err, idea){
         if(err){
             console.log(err);
@@ -151,7 +174,7 @@ app.get("/ideas/:id/comments/new", function(req, res){
 });
 
 //CREATE COMMENT
-app.post("/ideas/:id/comments", function(req, res){
+app.post("/ideas/:id/comments", isLoggedIn, function(req, res){
     var date = new Date();
     req.body.comment.date = date.getDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes();
    
@@ -204,6 +227,56 @@ app.delete("/ideas/:idea_id/comments/:comment_id", function(req, res){
         }
     });
 });
+
+// ============
+// AUTH ROUTES
+// ============
+
+// Show Sign Up Form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+// Sign Up logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.redirect("/register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/ideas");
+        });
+    });
+});
+
+// Show Login Form
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+// Login logic
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/ideas",
+    failureRedirect: "/login"
+}), function(req, res){
+    
+})
+
+// Logout route
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/ideas");
+});
+
+// IS LOGGEDIN MIDDLEWARE
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+} 
 
 app.listen("3000", function(){
     console.log("Lightbulb is Running");
