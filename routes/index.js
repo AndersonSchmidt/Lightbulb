0953f7911122a2bccd668d3f58510e61e64d54f6
Anderson Schmidt <schmidtanderson@hotmail.com>
2019-05-13 14:48:30 -0300

    Adding User Profile

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
 var express  = require("express");
 var router   = express.Router();
+var multer   = require("multer");
 var User     = require("../models/user");
+var Idea     = require("../models/idea");
 var passport = require("passport");
+var ObjectId = require('mongoose').Types.ObjectId;
+
+// MULTER SETTINGS
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/images');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+var upload = multer({ storage: storage });
 
 // MAIN PAGE
 router.get("/", function(req, res){
@@ -18,8 +32,11 @@ router.get("/register", function(req, res){
 });
 
 // Sign Up logic
-router.post("/register", function(req, res){
+router.post("/register", upload.single("image"), function(req, res){
     var newUser = new User({username: req.body.username});
+
+    newUser.avatarPath = req.file.path.replace('public\\', '/');
+
     User.register(newUser, req.body.password, function(err, user){
         if(err){
             console.log(err);
@@ -49,5 +66,21 @@ router.get("/logout", function(req, res){
     req.logout();
     res.redirect("/ideas");
 });
+
+router.get("/users/:id", function(req, res){
+    User.findById(req.params.id, function(err, user){
+        if(err){
+
+        }else{
+            Idea.find({'user.id': ObjectId(req.params.id)}, function(err, ideas){
+                if(err){
+                    console.log(err);
+                }else{
+                    res.render("users/show", {user: user, ideas: ideas});
+                }
+            });
+        }
+    })
+}); 
 
 module.exports = router;
